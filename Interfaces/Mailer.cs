c51bf86659a2d0b3a2e7baf7b601b533b10b32fa
Dennis Jakupovic <d.jakupovic@evidos.nl>
2019-03-28 08:12:39 +0100

    Reformat spaces as tabs

--- Interfaces/Mailer.cs
+++ Interfaces/Mailer.cs
@@ -15,18 +15,18 @@ namespace JakupovicNL
 		public Mailer()
 		{
 			_apiKey = Environment.GetEnvironmentVariable(Startup._sendgridApiKey);
-            _client = new SendGridClient(_apiKey);
+			_client = new SendGridClient(_apiKey);
 		}
 
 		public async Task<Response> SendEmail(EmailForm form)
 		{
 			var from = new EmailAddress(form.EmailAddress, form.Name);
-            var subject = "Someone send you a message!";
-            var to = new EmailAddress("jakupovicdennis@gmail.com", "Nuss");
-            var plainTextContent = form.Message;
-            var htmlContent = $"<strong>{form.Message}</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response =  await _client.SendEmailAsync(msg);
+			var subject = "Someone send you a message!";
+			var to = new EmailAddress("jakupovicdennis@gmail.com", "Nuss");
+			var plainTextContent = form.Message;
+			var htmlContent = $"<strong>{form.Message}</strong>";
+			var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+			var response =  await _client.SendEmailAsync(msg);
 
 			return response;
 		}
